576aa8a41f1916b7d4da8be5c46c896a44887f18
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-05-23 22:37:58 -0400

    The time ain't chiptuning with this one

---
+++ js/remixes/TheTimeIsChiptuningNow.js
@@ -0,0 +1,8 @@
+// Made at: July 16 2024
+// Mode: Bytebeat
+// Sample rate: 8000
+
+// One plus one equals three
+// The Two plus Two is Five predecessor
+
+(t*(t&16384?7:5)*(4-(3&t>>9))>>(3&-t>>(t&(j=4096)?17:16))&128)*(-t&2047)/j+(random()*(-t&(a=j-1))/230*(t&j?5:0)&32)*(-t&a)/2048
