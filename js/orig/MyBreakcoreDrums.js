6650d74470380281a8be12bee0e2649051283fd3
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-05-30 10:38:53 -0400

    This is also not breakcore

---
+++ js/orig/MyBreakcoreDrums.js
@@ -0,0 +1,7 @@
+// Made at: August 2 2024
+// Mode: Bytebeat
+// Sample rate: 30720
+
+// What a strange sample rate bruh
+
+(3e5/(t&8191)&64)+((t*sin(t>>2)&255)*(-t&4095)/9e3)*"101"[(t>>12)%3]+random()*(-t&8191)/128*"0110"[t>>13&3]
