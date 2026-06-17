bba381acdec207f7bdde32f8d28f32dd416d6995
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-06-16 20:34:48 -0400

    This is hardcore for real
    Added "Epic Scene of a War", a bytebeat I made for no reason.

---
+++ js/remixes/EpicSceneOfAWar.js
@@ -0,0 +1,7 @@
+// Made at: September 6 2024
+// Mode: Bytebeat
+// Sample rate: 44100
+
+// This IS hardcore for real
+
+a=t%(t>>15^t/90%(t>>9)%(t&t>>4))-4,b=(sqrt(3e2*(t%(8192*"2122"[t>>15&3])))&64)+(3e5/(t&32767)&64)+(t*sin(t>>2)&255)*(-t&8191)/2e4*"001"[(t>>13)%3]+random()*(-t&8191)/2e2,b+(a/4&63)/2
