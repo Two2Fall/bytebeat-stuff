6818fe6efbedf4dd1b58ce73913972045f877f7d
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-06-16 20:41:38 -0400

    Hardcore for real
    Added "Absolute rock", a remix of Hell Rock by Kolbosa.

---
+++ js/remixes/AbsoluteCore.js
@@ -0,0 +1,7 @@
+// Made at: September 7 2024
+// Mode: Bytebeat
+// Sample rate: 44100
+
+// This is also hardcore, hmm I made this bytebeat well in my opinion
+
+a=t>>12*t/(t/8&(c=4096)?130:120)&(d=127)^(t>>(t/4&c?4:8)&(t/8&c?d:63)&(t/64&c?d:0))+5E4/(t/(t/8&c?4:6)&4095)&d,b=(sqrt(3e2*(t%(8192*"2122"[t>>15&3])))&64)+(3e5/(t&32767)&64)+(t*sin(t>>2)&255)*(e=-t&8191)/2e4*"001"[(t>>13)%3]+random()*e/2e2,b/2+a/2%128
