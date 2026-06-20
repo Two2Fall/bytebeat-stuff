b9f6ee1fa16dca1bb261109afdb790e6ca3f9152
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-06-20 11:40:09 -0400

    Thingaz Core
    Added "ThingCore", a hardcore bytebeat I made

---
+++ js/remixes/ThingCore.js
@@ -0,0 +1,7 @@
+// Made at: September 8 2024
+// Mode: Bytebeat
+// Sample rate: 44100
+
+// Thingaz core
+
+a=(sqrt(3e2*(t%(8192*"2122"[t>>15&3])))&64)+(3e5/(t&32767)&64)+(t*sin(t>>2)&255)*(e=-t&8191)/2e4*"001"[(t>>13)%3]+random()*e/2e2,64+(a/1.5+(t&t/2&t/4)*t/4E3/4%64)/1.5
