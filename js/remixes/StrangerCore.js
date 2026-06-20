5e597a2c8a44e2c1e2f9f96a924c9ecd5fa2e82d
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-06-20 11:44:30 -0400

    Strange with core
    Added "StrangerCore", another hardcore bytebeat I made

---
+++ js/remixes/StrangerCore.js
@@ -0,0 +1,7 @@
+// Made at: September 8 2024
+// Mode: Bytebeat
+// Sample rate: 32000
+
+// It sounds pretty cool
+
+j=t/4,a=(sqrt(3e2*(t%(8192*"2122"[t>>15&3])))&64)+(3e5/(t&32767)&64)+(t*sin(t>>2)&255)*(e=-t&8191)/2e4*"001"[(t>>13)%3]+random()*e/2e2,64+(a/1.5+j*(j^j+(j>>15|1)^(j-1280^j)>>10)/4%64)/1.5
