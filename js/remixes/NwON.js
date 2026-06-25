6fc5ce5447db613a6d95569261fb662aacd77e3f
Twell Evans <166918540+Two2Fall@users.noreply.github.com>
2026-06-25 12:53:48 -0400

    Nwon sounds pretty good imo
    Added "Now with Octal Numbers", the sequel to a song with hex numbers and without functions.

---
+++ js/remixes/NwON.js
@@ -0,0 +1,8 @@
+// Now with Octal Numbers
+// Made at: September 14 2024
+// Mode: Bytebeat
+// Sample rate: 44100
+
+// Hardcoring but without hardcore... why did I enable functions here?
+
+a=(b=(c=0o1)/0o4)*(d=0o2),t*d/d**(~t>>0o23&0o3)*[b*0o3,c+a,2+b][(t>>0o15)%3]*(c+[0,b,a,b][t>>0o21&0o3])%0o400*(-t&0o17777)/0o23420/d+(t>0o10014422)*(0o1111740/(t&0o37777)&0o100)+(t>0o10322510)*(t*sin(t>>d)&0o377)*(-t&0o77777)/0o277777*(t>>0o17&c)+(t>0o3775462)*(random()*(-t&0o7777)/0o200)
